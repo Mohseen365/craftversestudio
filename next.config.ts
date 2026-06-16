06a40edbcf41e3f475013e0bbd89571f53c733a9
Mohseen365 <mohammedmohsin365@gmail.com>
2026-06-17 01:06:24 +0530

    added pathname in config

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,12 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        pathname: "/**", // Allows all paths from Unsplash
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        pathname: "/**", // Allows all paths from Unsplash
       },
     ],
   },
